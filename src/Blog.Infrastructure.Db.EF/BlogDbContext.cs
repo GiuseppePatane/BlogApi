62a2d8338a8f0c8fc81a6d05b4fc4605c1784329
Giuseppe Patanè <gpatane13@gmail.com>
2022-02-09 15:25:35 +0100

    Working on  Blog Store logic

---
+++ src/Blog.Infrastructure.Db.EF/BlogDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Domain.Entities;
+using Blog.Infrastructure.Db.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infrastructure.Db.EF;
+
+public class BlogDbContext :DbContext 
+{
+     public BlogDbContext(DbContextOptions<BlogDbContext> options)
+        : base(options)
+    {
+    }
+     public  DbSet<Author> Authors { get; set; }
+     public  DbSet<Category>Categories { get; set; }
+     public  DbSet<Tag> Tags { get; set; }
+     public  DbSet<BlogPost>BlogPosts { get; set; }
+
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new TagConfiguration());
+        modelBuilder.ApplyConfiguration(new TagConfiguration());
+        modelBuilder.ApplyConfiguration(new TagXBlogPostConfiguration());
+    }
+}
